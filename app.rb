cea201a26c799236212ee18df467981e067ff462
Tracie Masek <42948851+traciemasek@users.noreply.github.com>
2019-08-24 14:54:26 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do 
+    "My name is Tracie"
+  end
+
+  get '/hometown' do 
+    "My hometown is Westerville, Ohio"
+  end
+
+  get '/favorite-song' do  
+    "My favorite song is 'Africa' by Toto"
+  end
+
+
+
 end
